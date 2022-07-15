f01bce075093e57b652f5c8f8e8bcdaada7291a4
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-07-15 21:35:28 +0100

    update colors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,25 +1,25 @@
 import { Link } from "react-router-dom";
 import Input from "../components/Input";
-import logo from '../assets/favicon.svg'
+// import logo from '../assets/logo.png'
 
 const Login = () => {
   return (
     <main className="flex h-screen w-screen">
       {/* ***responsive marker */}
-    <div className="h-full w-3/4 bg-red-500 hidden md:block">1</div>
+    <div className="h-full w-3/4 bg-primary hidden md:block">1</div>
 
     <div className="h-screen relative w-full p-12">
       {/* ***responsive marker */}
     <div className="m-auto md:p-10 md:w-full" >
       <div className="absolute top-0 right-0 p-4 text-xs">
         Not Registered?{" "}
-        <Link to="/signup" className="text-red-500 hover:underline">
+        <Link to="/signup" className="text-primary hover:underline">
           Create an Account
         </Link>{" "}
       </div>
       <div className="">
         <Link to="/">
-          <img src={logo} alt="cardepay" width={50} className="logo" />
+          {/* <img src={logo} alt="cardepay" width={150} className="logo" /> */}
         </Link>
         <h1 className="font-bold text-3xl py-2">Login</h1>
         <p className="text-xs">
@@ -41,12 +41,12 @@ const Login = () => {
           id="password"
         />
 
-        <button className="p-3 py-4 text-white bg-red-500 text-xs w-full transition-all hover:bg-red-400 hover:text-black">
+        <button className="p-3 py-4 text-white bg-primary text-xs w-full transition-all hover:text-black">
           Login
         </button>
         <Link
           to="/forgotten-password"
-          className="my-4 block text-xs  transition-all text-red-500 hover:underline "
+          className="my-4 block text-xs  transition-all text-primary hover:underline "
         >
           Forgotten Password ?
         </Link>
