bea619852ea3a39d2868f6f4625748c51796aaae
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-07-16 18:25:20 -0700

    404 page completed, animation added to the nav menu and some extensions change

--- src/pages/page-not-found.jsx
+++ src/pages/page-not-found.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BsHouseFill } from "react-icons/bs";
 import notfound from '../assets/404.png';
 import logo from '../assets/logo.png'
