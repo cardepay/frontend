11f39c5c998375a0731d0078b6d9b90b9672c6bc
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-07-20 15:31:56 -0700

    fixed the contact and input issue, however, signup page refuses to load

---
+++ src/components/input/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ReactNode, useState } from 'react'
+/*
+  @TODO: Properly integrate InputHandler ( func )
+*/
+interface InputProps{
+label:string;
+type:string;
+errorMsg?:string | ReactNode;
+id:string;
+placeholder?:string
+}
+
+const Input = ({label, type, id, placeholder,errorMsg}:InputProps) => {
+  const [error, triggerError]= useState(false);
+
+  // testing handler
+  const InputHandler = (e)=>{
+    if(e.target.value.length < 6) triggerError(true)
+    else triggerError(false)
+  }
+  return (
+    <div className="grid gap-1 pb-7">
+    <label htmlFor={id} className="text-sm">{label}</label>
+    <input type={type} id={id} placeholder={placeholder} onChange={InputHandler} className={`ring-1 outline-none p-3 text-xs transition-all ${error ? 'ring-red' : 'ring-black/25 focus:ring-black/50'}`}/>
+    <p className={`text-red transition-all select-none text-xs ${!error && 'hidden'}`}>{ errorMsg || 'Incorrect ' + label }</p>
+</div>
+  )
+}
+
+export default Input 
--- src/pages/auth-pages/signup.jsx
+++ src/pages/auth-pages/signup.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { FaLock , FaUser, FaPhone, FaEnvelope, FaGlobe, FaBriefcase} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-import { Input } from '../../components'
-import { InputIcon } from '../../components/Input'
-//import { Link } from 'react-router-dom'
-//import Input from '../../components/Input'
 
 const Signup = () => {
   return (
