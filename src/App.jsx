21167ece05b475c8d64dfa8b8eb9cd3dd4138ee3
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-03 14:04:57 +0100

    Design: Forgotten Password

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes,Route } from 'react-router-dom';
 import Home from './pages/home';
 import SignUp from './pages/signUp';
 import Login from './pages/login';
+import ForgottenPassword from './pages/forgotten-password';
 import PageNotFound from './pages/page-not-found';
 
 
@@ -14,6 +15,7 @@ const App = () => {
 	<Route exact path='/' element={< Home />} />
 	<Route exact path='/signup' element={< SignUp />} />
 	<Route exact path='/login' element={< Login />} />
+	<Route exact path='/forgotten-password' element={< ForgottenPassword />} />
 
 
 
