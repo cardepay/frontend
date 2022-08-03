f7300806b12c6e3c8c17fc7cb73b56eb58e40faa
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-03 16:25:07 +0100

    Feat:create card on Hero section

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -2,10 +2,10 @@ import Input from '../components/Input'
 import {MdAlternateEmail, MdOutlineMail}from 'react-icons/md'
 import fw from '../assets/flutterwave.svg';
 import pp from '../assets/paypal.svg';
-
+import Card from '../components/Card'
 const Hero = () => {
   return (
-    <div className='min-h-screen flex items-center'>
+    <main className='min-h-screen flex items-center' id='hero'>
       <div className='p-12'>
       <h1 className='py-4 text-5xl font-medium'>
             <span className='block'>Your future</span>
@@ -14,7 +14,7 @@ const Hero = () => {
         <p className='text-sm w-[500px] my-3'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea fuga possimus voluptatem quo doloribus sequi repellat corporis veritatis soluta
         </p>
-        <p className='px-1 text-xs'>JOIN OUR WAITLIST</p>
+        <p className='px-5 py-1 text-xs text-[10px] rounded-full bg-primary inline-block'>JOIN OUR WAITLIST</p>
         <Input type='email' label='Email' id='last-name' placeholder='JohnDoe@gmail.com' icon={<MdOutlineMail/>}  />
         <div className='my-6 select-none pointer-events-none'>
         <span className='text-sm opacity-80'>INTEGRATIONS:</span>
@@ -27,9 +27,10 @@ const Hero = () => {
 
 
       <div>
-
+    <Card />
+    <Card />
           </div>
-    </div>
+    </main>
   )
 }
 
