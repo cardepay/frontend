662cd9d7627ae4d3c50f4e18b039a1be3f71638e
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-03 12:05:57 +0100

    Fix: 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
 import { Routes,Route } from 'react-router-dom';
 import Home from './pages/home';
+import PageNotFound from './pages/page-not-found';
 
 
 const App = () => {
   return (
 	<Routes>
 	<Route exact path='/' element={< Home />} />
+
+
+
+  
+	<Route exact path='*' element={< PageNotFound />} />
 </Routes>
   )
 }
