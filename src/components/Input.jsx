e3b046cb4a876c3323a0fcfd6225075c26c1920a
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-08-04 11:52:00 +0100

    Fix: Homepage responsive

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
-const Input = ({width,onChange,label,id,name,type, icon,placeholder}) => {
+const Input = ({onChange,label,id,name,type, icon,placeholder}) => {
   return (
-    <div style={{width:width && '100%'}} className='bg-gray-700 p-2 my-3 rounded-[20px] relative'>
+    <div className='bg-gray-700 w-full p-2 my-3 rounded-[20px] relative'>
     <label className='px-3 text-xs text-white opacity-80'>{label}</label>
    <div className='px-3 flex items-center justify-between w-full'>
    <input type={type} id={id} name={name} placeholder={placeholder || ''} onChange={onChange}  className='w-full outline-none text-white bg-transparent'/>
--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -5,20 +5,24 @@ import pp from '../assets/paypal.svg';
 import Card from '../components/Card'
 const Hero = () => {
   return (
-    <main className='min-h-screen flex items-center' id='hero'>
+    <main className='py-6 flex relative flex-col md:flex-row items-center' id='hero'>
       <div className='p-12'>
-      <h1 className='py-4 text-5xl font-medium'>
+      <h1 className='py-4 text-5xl text-center md:text-left font-medium'>
             <span className='block'>Your future</span>
             <span className='block'><span className='primary'>payment</span> is here.</span>
         </h1>
-        <p className='text-sm w-[500px] my-3'>
+        <p className='text-sm max-w-[500px] text-center md:text-left my-3'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea fuga possimus voluptatem quo doloribus sequi repellat corporis veritatis soluta
         </p>
-        <p className='px-5 py-1 text-xs text-[10px] rounded-full bg-primary inline-block'>JOIN OUR WAITLIST</p>
-        <Input type='email' label='Email' id='last-name' placeholder='JohnDoe@gmail.com' icon={<MdOutlineMail/>}  />
-        <div className='my-6 select-none pointer-events-none'>
+       <div className="text-center md:text-left ">
+          <p className='px-5 py-1 text-xs text-[10px] rounded-full bg-primary inline-block '>JOIN OUR WAITLIST</p>
+       </div>
+      <div className="relative">
+      <Input type='email' label='Email' id='last-name' placeholder='JohnDoe@gmail.com' icon={<MdOutlineMail/>}  />
+      </div>
+        <div className='my-6 text-center md:text-left  select-none pointer-events-none'>
         <span className='text-sm opacity-80'>INTEGRATIONS:</span>
-        <div className='flex gap-x-3'>
+        <div className='flex gap-x-3 justify-center md:justify-start '>
         <img src={fw} width={150} />
         <img src={pp} width={100} />
         </div>
@@ -26,7 +30,7 @@ const Hero = () => {
           </div>
 
 
-      <div>
+      <div className='scale-[0.8]'>
     <Card />
     <Card />
           </div>
